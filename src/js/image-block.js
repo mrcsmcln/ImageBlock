b519be2d43612d8df9a148b4da85fd6c9e291bb8
Marcus McLean <marcus@mclean.digital>
2015-11-19 00:13:31 -0500

    Changed vesion number

--- src/js/image-block.js
+++ src/js/image-block.js
@@ -12,7 +12,7 @@
         this.resizeImages()
     }
 
-    ImageBlock.VERSION = '1.0.6'
+    ImageBlock.VERSION = '1.0.7'
 
     ImageBlock.prototype.handleResize = function() {
         this.getBlockDimensions()
