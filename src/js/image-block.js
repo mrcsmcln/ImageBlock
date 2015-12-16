82bfd0576b269d65072aa794495f40ad1b5412fd
Marcus McLean <mmclean@160over90.com>
2015-12-16 13:32:17 -0500

    1.0.13

--- src/js/image-block.js
+++ src/js/image-block.js
@@ -24,7 +24,7 @@
         }, this))
     }
 
-    ImageBlock.VERSION = '1.0.12'
+    ImageBlock.VERSION = '1.0.13'
 
     ImageBlock.prototype.handleResize = function() {
         this.getBlockDimensions()
